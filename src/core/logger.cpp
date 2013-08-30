e2a787db79afb871ab7ed7af5412459a25a92e25
lucas8 <luc.linux@mailoo.org>
2013-08-30 22:23:51 +0200

    Added logging functions.

---
+++ src/core/logger.cpp
@@ -0,0 +1,100 @@
+
+#include "core/logger.hpp"
+#include "core/systemtime.hpp"
+#include <vector>
+#include <sstream>
+
+namespace core
+{
+    namespace logger
+    {
+        /* Global variables difinition */
+        struct _output {
+            std::ostream* os;
+            bool todel;
+        };
+        static std::vector<_output> outputs;
+        static unsigned int blocks;
+        static Level lvlmin;
+
+        void init()
+        {
+            blocks = 0;
+            lvlmin = MSG;
+        }
+
+        void free()
+        {
+            for(size_t i = 0; i < outputs.size(); ++i) {
+                if(outputs[i].todel)
+                    delete outputs[i].os;
+            }
+        }
+
+        void addOutput(std::ostream* os, bool todel)
+        {
+            _output op;
+            op.os = os;
+            op.todel = todel;
+            outputs.push_back(op);
+        }
+
+        void pushBlock()
+        {
+            ++blocks;
+        }
+
+        void popBlock()
+        {
+            if(blocks > 0)
+                --blocks;
+        }
+
+        void minLevel(Level min)
+        {
+            lvlmin = min;
+        }
+
+        void log(const std::string& msg, Level lvl, bool date)
+        {
+            if(lvl < lvlmin)
+                return;
+
+            std::ostringstream whole; /* Complete msg (with date, blocks and level indication */
+
+            if(date) {
+                SystemTime st; /* Constructor to local time */
+                whole << "[" << st.format("%m/%d/%Y %H:%M:%S", 20) << "] ";
+            }
+
+            switch(lvl) {
+                case MSG:
+                    whole << "        ";
+                    break;
+                case WARNING:
+                    whole << "  /!\\   ";
+                    break;
+                case ERROR:
+                    whole << "  (E)   ";
+                    break;
+                case SERIOUS:
+                    whole << " (!E!)  ";
+                    break;
+                case FATAL:
+                    whole << "//!E!\\\\ ";
+                    break;
+            }
+
+            for(size_t i = 0; i < blocks; ++i)
+                whole << ">";
+            whole << " " << msg;
+
+            for(size_t i = 0; i < outputs.size(); ++i) {
+                (*outputs[i].os) << whole.str() << std::endl;
+            }
+        }
+
+    }
+}
+
+
---
+++ src/core/logger.hpp
@@ -0,0 +1,48 @@
+
+#ifndef DEF_CORE_LOGGER
+#define DEF_CORE_LOGGER
+
+#include <ostream>
+#include <string>
+
+namespace core
+{
+    namespace logger
+    {
+        /* Init logging, must be called only once */
+        void init();
+        /* Free everything */
+        void free();
+
+        /* Add output
+         * If todel, the ostream will be delete'd*/
+        void addOutput(std::ostream* os, bool todel = false);
+
+        /* Blocks manipulation : one more block will add a '>' at the beggining of the line */
+        void pushBlock();
+        void popBlock();
+
+        /* Print a message */
+        enum Level {
+            MSG,     /* Simple info */
+            WARNING, /* Warning */
+            ERROR,   /* Error not fatal, will not really matter the execution*/
+            SERIOUS, /* Error not fatal, but will disturb the execution */
+            FATAL    /* Fatal error */
+        };
+        /* Level from where to print 
+         * ex : if min == ERROR, will print ERROR and FATAL
+         */
+        void minLevel(Level min);
+
+        /* msg will be displayed to all the outputs given,
+         * If lvl < min, msg will not be displayed
+         * msg must be only one line
+         * if date = false, the date won't be showed
+         */
+        void log(const std::string& msg, Level lvl, bool date = true);
+    }
+}
+
+#endif
+
