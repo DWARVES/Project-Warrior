5b9daaef944f893ee69824f8bf095df251c4149a
lucas8 <luc.linux@mailoo.org>
2013-09-19 21:56:06 +0200

    Added basic OpenGL extensions loading system.

---
+++ src/graphics/exts.cpp
@@ -0,0 +1,53 @@
+
+#include "graphics/exts.hpp"
+#include <GL/glu.h>
+#include <SDL.h>
+#include <cstring>
+
+namespace graphics
+{
+    namespace internal
+    {
+        ext_not_loaded::ext_not_loaded(const char* mname) throw()
+        {
+            m_name = mname;
+        }
+
+        const char* ext_not_loaded::what() const throw()
+        {
+            return "Tryed to use an OpenGL extension function which was not loaded.";
+        }
+
+        const char* ext_not_loaded::name() const throw()
+        {
+            return m_name;
+        }
+
+
+        Extensions::Extensions()
+            : m_exts((const char*)glGetString(GL_EXTENSIONS))
+        {}
+
+        bool Extensions::has(const std::string& name) const
+        {
+            return strstr(m_exts, name.c_str()) != NULL;
+        }
+
+        bool Extensions::load(const std::string& name)
+        {
+            /* The function was already loaded, so no need to load it again */
+            if(isLoaded(name))
+                return true;
+            m_funcs[name] = SDL_GL_GetProcAddress(name.c_str());
+            return true;
+        }
+
+        bool Extensions::isLoaded(const std::string& name) const
+        {
+            return m_funcs.find(name) != m_funcs.end();
+        }
+
+    }
+}
+
+
---
+++ src/graphics/exts.hpp
@@ -0,0 +1,69 @@
+
+#ifndef DEF_GRAPHICS_EXTS
+#define DEF_GRAPHICS_EXTS
+
+#include <string>
+#include <map>
+#include <exception>
+#include <sstream>
+#include "core/logger.hpp"
+
+namespace graphics
+{
+    namespace internal
+    {
+        /* Exception class for Extensions */
+        class ext_not_loaded : public std::exception
+        {
+            public:
+                ext_not_loaded() = delete;
+                ext_not_loaded(const char* mname) throw();
+
+                virtual const char* what() const throw();
+                /* Returns the name of the openGL function which couldn't be loaded */
+                const char* name() const throw();
+
+            private:
+                const char* m_name;
+        };
+
+        /* Manages OpenGL extensions */
+        class Extensions
+        {
+            public:
+                Extensions();
+                Extensions(const Extensions&) = delete;
+
+                /* Check if the hardware supports an extension */
+                bool has(const std::string& name) const;
+                /* Load a function from an extension */
+                bool load(const std::string& name);
+                bool isLoaded(const std::string& name) const;
+
+                /* Returns a pointer to an extension function which must be loaded
+                 * T is the name of the ptr type in opengl (like PFNSOMETHINGPROC)
+                 * Will throw an ext_not_loaded exception if the function was not loaded
+                 */
+                template<typename T> T operator()(const std::string& name) const;
+
+            private:
+                const char* m_exts;
+                std::map<std::string, void*> m_funcs;
+        };
+                
+        template<typename T> T Extensions::operator()(const std::string& name) const
+        {
+            if(!isLoaded(name)) {
+                std::ostringstream oss;
+                oss << "Tryed to call an OpenGL extension function which was not loaded : " << name;
+                core::logger::logm(oss.str(), core::logger::ERROR);
+                throw ext_not_loaded(name.c_str());
+            }
+
+            return dynamic_cast<T>(m_funcs.at(name));
+        }
+    }
+}
+
+#endif
+
