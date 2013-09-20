c1daf2ebc01474c2e7482b442d3e74deb369ba20
lucas8 <luc.linux@mailoo.org>
2013-09-20 23:45:47 +0200

    New implementation of openGL extensions : using GLEW.

--- src/graphics/exts.cpp
+++ src/graphics/exts.cpp
@@ -1,72 +1,32 @@
 
 #include "graphics/exts.hpp"
-#include <GL/glu.h>
+#include "core/logger.hpp"
 #include <SDL.h>
-#include <string>
+#include <sstream>
 
 namespace graphics
 {
     namespace internal
     {
-        ext_not_loaded::ext_not_loaded(const char* mname) throw()
-        {
-            m_name = mname;
-        }
-
-        const char* ext_not_loaded::what() const throw()
-        {
-            return "Tryed to use an OpenGL extension function which was not loaded.";
-        }
-
-        const char* ext_not_loaded::name() const throw()
-        {
-            return m_name;
-        }
-
-
         Extensions::Extensions()
-            : m_exts(NULL)
         {}
                 
-        bool Extensions::loadList()
+        bool Extensions::init()
         {
-            m_exts = (const char*)glGetString(GL_EXTENSIONS);
-            if(m_exts == NULL)
+            GLenum err = glewInit();
+            if(err != GLEW_OK) {
+                std::ostringstream oss;
+                oss << "Coudn't init GLEW for openGL extension usage : \"" << glewGetErrorString(err) << "\"";
+                core::logger::logm(oss.str(), core::logger::WARNING);
                 return false;
+            }
             else
                 return true;
         }
 
         bool Extensions::has(const std::string& name) const
         {
-            return strstr(m_exts, name.c_str()) != NULL;
-        }
-
-        bool Extensions::load(const std::string& name)
-        {
-            /* The function was already loaded, so no need to load it again */
-            if(isLoaded(name))
-                return true;
-            m_funcs[name] = SDL_GL_GetProcAddress(name.c_str());
-
-            if(!m_funcs[name]) {
-                std::ostringstream oss;
-                oss << "Couldn't get the openGL extension function " << name;
-                core::logger::logm(oss.str(), core::logger::WARNING);
-                m_funcs.erase(name);
-                return false;
-            }
-            else {
-                std::ostringstream oss;
-                oss << "Loaded the openGL extension function " << name;
-                core::logger::logm(oss.str(), core::logger::MSG);
-                return true;
-            }
-        }
-
-        bool Extensions::isLoaded(const std::string& name) const
-        {
-            return m_funcs.find(name) != m_funcs.end();
+            return glewIsSupported(name.c_str());
         }
     }
 }
--- src/graphics/exts.hpp
+++ src/graphics/exts.hpp
@@ -3,30 +3,13 @@
 #define DEF_GRAPHICS_EXTS
 
 #include <string>
-#include <map>
-#include <exception>
-#include <sstream>
-#include "core/logger.hpp"
+#include <GL/glew.h>
+#include <GL/glu.h>
 
 namespace graphics
 {
     namespace internal
     {
-        /* Exception class for Extensions */
-        class ext_not_loaded : public std::exception
-        {
-            public:
-                ext_not_loaded() = delete;
-                ext_not_loaded(const char* mname) throw();
-
-                virtual const char* what() const throw();
-                /* Returns the name of the openGL function which couldn't be loaded */
-                const char* name() const throw();
-
-            private:
-                const char* m_name;
-        };
-
         /* Manages OpenGL extensions */
         class Extensions
         {
@@ -35,41 +18,10 @@ namespace graphics
                 Extensions(const Extensions&) = delete;
 
                 /* Get the list of supported extensions, must be the first function called */
-                bool loadList();
+                bool init();
                 /* Check if the hardware supports an extension */
                 bool has(const std::string& name) const;
-                /* Load a function from an extension */
-                bool load(const std::string& name);
-                bool isLoaded(const std::string& name) const;
-
-                /* Returns a pointer to an extension function which must be loaded
-                 * Will throw an ext_not_loaded exception if the function was not loaded
-                 */
-                template<typename T> T call(const std::string& name) const;
-
-            private:
-                const char* m_exts;
-                std::map<std::string, void*> m_funcs;
         };
-                
-        template<typename FN> union PtrToFn {
-            FN fn;
-            void* ptr;
-        };
-
-        template<typename T> T Extensions::call(const std::string& name) const
-        {
-            if(!isLoaded(name)) {
-                std::ostringstream oss;
-                oss << "Tryed to call an OpenGL extension function which was not loaded : " << name;
-                core::logger::logm(oss.str(), core::logger::ERROR);
-                throw ext_not_loaded(name.c_str());
-            }
-
-            PtrToFn<T> tr;
-            tr.ptr = m_funcs.at(name);
-            return tr.fn;
-        }
     }
 }
 
