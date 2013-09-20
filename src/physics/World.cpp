8753c1b1d3a0dc31fef614746644daf62243c626
Champitoad <pablo.donato@mailoo.org>
2013-09-20 23:26:39 +0200

    Added name verification in physics::World and physics::Entity.

--- src/physics/World.cpp
+++ src/physics/World.cpp
@@ -1,5 +1,6 @@
 
 #include "World.hpp"
+#include "core/logger.hpp"
 
 namespace physics
 {
@@ -10,12 +11,26 @@ namespace physics
 
     Entity* World::getEntity(const std::string& name) const
     {
+        if(!m_entities.count(name))
+        {
+            core::logger::logm("Tried to get unexisting entity \"" + name + "\" : returned NULL.", core::logger::WARNING);
+            return NULL;
+        }
+
         return m_entities.at(name);
     }
 
     Entity* World::createEntity(const std::string& name, const geometry::Point& position, const b2BodyType& bodyType, bool fixedRotation)
     {
-        m_entities[name] = new Entity(m_world, position, bodyType, fixedRotation);
+        if(m_entities.count(name))
+        {
+            core::logger::logm("Tried to override the existing entity \"" + name + "\" : cancelled operation and returned NULL.", core::logger::WARNING);
+            return NULL;
+        }
+
+        m_entities[name] = new Entity(name, m_world, position, bodyType, fixedRotation);
+        core::logger::logm("The entity \"" + name + "\" has been created.", core::logger::MSG);
+
         return m_entities.at(name);
     }
 }
--- tests/physics/Entity-test/testbed/EntityTest.h
+++ tests/physics/Entity-test/testbed/EntityTest.h
@@ -2,6 +2,8 @@
 #define DEF_ENTITYTEST_H
 
 #include "physics/Entity.hpp"
+#include "core/logger.hpp"
+#include <iostream>
 
 #define ROTATION_FIXED true
 #define ROTATION_NOT_FIXED false
@@ -17,16 +19,16 @@ class EntityTest : public Test
 
         EntityTest()
         {
-            //square = new Entity(m_world, Point(-300, 50), b2_dynamicBody, Entity::Type::Any, Entity::Type::Any, 0, ROTATION_NOT_FIXED);
-            square = new Entity(m_world, Point(-300, 50), b2_dynamicBody, Entity::Type::Character, ~Entity::Type::Platform, 0, ROTATION_NOT_FIXED);
+            core::logger::init();
+            core::logger::addOutput(&std::cout);
+
+            square = new Entity("Character", m_world, Point(-300, 50), b2_dynamicBody, Entity::Type::Character, ~Entity::Type::Platform, 0, ROTATION_NOT_FIXED);
             square->createFixture("body", AABB(100, 100), 1, 1);
 
-            //circle = new Entity(m_world, Point(-100, 50), b2_dynamicBody, Entity::Type::Any, Entity::Type::Any, 1, ROTATION_NOT_FIXED); // SPECTRE ATTACK
-            circle = new Entity(m_world, Point(-100, 50), b2_dynamicBody, Entity::Type::Attack, ~(Entity::Type::Obstacle | Entity::Type::Platform), 1, ROTATION_NOT_FIXED); // SPECTRE ATTACK
+            circle = new Entity("Spectre attack", m_world, Point(-100, 50), b2_dynamicBody, Entity::Type::Attack, ~(Entity::Type::Obstacle | Entity::Type::Platform), 1, ROTATION_NOT_FIXED); // SPECTRE ATTACK
             circle->createFixture("body", Circle(50), 1, 1);
 
-            //polygon = new Entity(m_world, Point(100, 50), b2_dynamicBody, Entity::Type::Any, Entity::Type::Any, 1, ROTATION_NOT_FIXED); // GHOST ATTACK
-            polygon = new Entity(m_world, Point(100, 50), b2_dynamicBody, Entity::Type::Attack, ~(Entity::Type::Character | Entity::Type::Platform), 1, ROTATION_NOT_FIXED); // GHOST ATTACK
+            polygon = new Entity("Ghost attack", m_world, Point(100, 50), b2_dynamicBody, Entity::Type::Attack, ~(Entity::Type::Character | Entity::Type::Platform), 1, ROTATION_NOT_FIXED); // GHOST ATTACK
             std::vector<Point> vertices;
             vertices.push_back(Point(-25, 0));
             vertices.push_back(Point(0, -50));
@@ -34,8 +36,15 @@ class EntityTest : public Test
             vertices.push_back(Point(0, 50));
             polygon->createFixture("body", Polygon(vertices), 1, 1);
 
-            Entity ground(m_world, Point(0, 0), b2_staticBody, Entity::Type::Obstacle);
+            Entity ground("Ground", m_world, Point(0, 0), b2_staticBody, Entity::Type::Obstacle);
             ground.createFixture("body", Line(Point(0, 0), Point(2500, 0), Point(1250, 0)), 1, 1);
+            ground.createFixture("body", Line(Point(0, 0), Point(2500, 0), Point(1250, 0)), 1, 1);
+            b2Fixture* f = ground.getFixture("pouet");
+        }
+
+        ~EntityTest()
+        {
+            core::logger::free();
         }
 
         void Step(Settings* settings)
