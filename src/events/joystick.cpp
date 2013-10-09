6257b528b5b9efc8f7c70a8246a79fd9e46093b6
lucas8 <luc.linux@mailoo.org>
2013-10-09 20:58:36 +0200

    Added an events::Joystick class.

---
+++ src/events/joystick.cpp
@@ -0,0 +1,93 @@
+
+#include "joystick.hpp"
+#include "core/logger.hpp"
+#include <sstream>
+
+namespace events
+{
+    Joystick::Joystick(JoystickID ID)
+        : m_joy(NULL)
+    {
+        m_joy = SDL_JoystickOpen(ID);
+        if(m_joy == NULL) {
+            std::ostringstream oss;
+            oss << "Couldn't open joystick #" << ID << " : \"" << SDL_GetError() << "\".";
+            core::logger::logm(oss.str(), core::logger::WARNING);
+            return; /* FIXME should launch an exception */
+        }
+        else {
+            std::ostringstream oss;
+            oss << "Joystick #" << ID << " opened.";
+            core::logger::logm(oss.str(), core::logger::MSG);
+        }
+    }
+
+    Joystick::~Joystick()
+    {
+        if(m_joy != NULL)
+            SDL_JoystickClose(NULL);
+    }
+
+    bool Joystick::operator==(JoystickID cmp) const
+    {
+        return id() == cmp;
+    }
+
+    bool Joystick::operator!=(JoystickID cmp) const
+    {
+        return id() != cmp;
+    }
+
+    Joystick::operator JoystickID() const
+    {
+        return id();
+    }
+
+    int Joystick::numAxis() const
+    {
+        return SDL_JoystickNumAxes(m_joy);
+    }
+
+    int Joystick::numButtons() const
+    {
+        return SDL_JoystickNumButtons(m_joy);
+    }
+
+    int Joystick::numHats() const
+    {
+        return SDL_JoystickNumHats(m_joy);
+    }
+
+    std::string Joystick::name() const
+    {
+        const char* n = SDL_JoystickName(m_joy);
+        if(n == NULL)
+            return std::string("");
+        else
+            return std::string(n);
+    }
+
+    JoystickID Joystick::id() const
+    {
+        return SDL_JoystickInstanceID(m_joy);
+    }
+
+    JoyHatState Joystick::hat(int h) const
+    {
+        return (JoyHatState)SDL_JoystickGetHat(m_joy, h);
+    }
+
+    int Joystick::axis(int a) const
+    {
+        return SDL_JoystickGetAxis(m_joy, a);
+    }
+
+    bool Joystick::button(int b) const
+    {
+        return SDL_JoystickGetButton(m_joy, b);
+    }
+
+}
+
+
+
---
+++ src/events/joystick.hpp
@@ -0,0 +1,62 @@
+
+#ifndef DEF_EVENTS_JOYSTICK
+#define DEF_EVENTS_JOYSTICK
+
+#include <SDL.h>
+#include <string>
+
+namespace events
+{
+    typedef int JoystickID;
+
+    enum class JoyHatState : Uint8 {
+        Center    = SDL_HAT_CENTERED,
+        Up        = SDL_HAT_UP,
+        Right     = SDL_HAT_RIGHT,
+        Down      = SDL_HAT_DOWN,
+        Left      = SDL_HAT_LEFT,
+        RightUp   = SDL_HAT_RIGHTUP,
+        RightDown = SDL_HAT_RIGHTDOWN,
+        LeftUp    = SDL_HAT_LEFTUP,
+        LeftDown  = SDL_HAT_LEFTDOWN,
+        /* UpRight == RightUp, UpLeft == LeftUp ... only present for user conveniance */
+        UpRight   = SDL_HAT_RIGHTUP,
+        UpLeft    = SDL_HAT_LEFTUP,
+        DownRight = SDL_HAT_RIGHTDOWN,
+        DownLeft  = SDL_HAT_LEFTDOWN,
+    };
+
+    class Joystick
+    {
+        /* FIXME : ball system is not supported */
+        public:
+            Joystick(JoystickID ID);
+            Joystick() = delete;
+            Joystick(const Joystick&) = delete;
+            ~Joystick();
+
+            /* Comparison */
+            bool operator==(JoystickID cmp) const;
+            bool operator!=(JoystickID cmp) const;
+            operator JoystickID() const;
+
+            /* Querying informations about the device */
+            int numAxis() const;
+            int numButtons() const;
+            int numHats() const;
+            std::string name() const;
+            JoystickID id() const;
+
+            /* Getting state of the device */
+            JoyHatState hat(int h) const;
+            /* The value is between -32768 and 32767 */
+            int axis(int a) const;
+            bool button(int b) const;
+
+        private:
+            SDL_Joystick* m_joy;
+    };
+}
+
+#endif
+
