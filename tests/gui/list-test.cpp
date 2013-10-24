27413960ab53fc0317cf6add8b1f343104988530
lucas8 <luc.linux@mailoo.org>
2013-10-24 22:37:24 +0200

    Updated test of gui::List.

--- tests/gui/list-test.cpp
+++ tests/gui/list-test.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include "gui/gui.hpp"
 #include "gui/widget.hpp"
-#include "gui/item.hpp"
+#include "gui/list.hpp"
 #include "graphics/graphics.hpp"
 #include "graphics/color.hpp"
 #include "geometry/aabb.hpp"
@@ -41,19 +41,37 @@ int main()
     gfx->loadTexture("ls", "guirc/item/ls.png");
     gfx->loadFont   ("fs", "guirc/item/fs.png");
 
-    /* Loading the item */
-    gui::internal::Item item(gfx);
-    item.setPart(gui::internal::Item::Right,  false, "r");
-    item.setPart(gui::internal::Item::Middle, false, "m");
-    item.setPart(gui::internal::Item::Left,   false, "l");
-    item.setPart(gui::internal::Item::Font,   false, "f");
-    item.setPart(gui::internal::Item::Right,  true,  "rs");
-    item.setPart(gui::internal::Item::Middle, true,  "ms");
-    item.setPart(gui::internal::Item::Left,   true,  "ls");
-    item.setPart(gui::internal::Item::Font,   true,  "fs");
-    item.text("HELLO WORLD!");
-    item.width(600.0f);
-    item.height(70.0f);
+    /* Loading the list */
+    gui::List list(gfx);
+    list.setPart(gui::List::Right,  false, "r");
+    list.setPart(gui::List::Middle, false, "m");
+    list.setPart(gui::List::Left,   false, "l");
+    list.setPart(gui::List::Font,   false, "f");
+    list.setPart(gui::List::Right,  true,  "rs");
+    list.setPart(gui::List::Middle, true,  "ms");
+    list.setPart(gui::List::Left,   true,  "ls");
+    list.setPart(gui::List::Font,   true,  "fs");
+    list.setItemSize(geometry::AABB(500.0f,50.0f));
+
+    /* Loading the items */
+    list.addItem(0, "Last", 30.0f);
+    list.addItem(0, "Unte", -5.0f);
+    list.addItem(0, "Hy everybody out of here");
+    list.addItem(0, "Hello world", 15.0f);
+    list.addItem(0, "Raspberry PI", 50.0f);
+    list.addItem(0, "@Centered widget !", 0.0f);
+    list.addItem(0, "Middle2", 10.0f);
+    list.addItem(0, "Middle1", -40.0f);
+    list.addItem(0, "Linux news and advices", -50.0f);
+    list.addItem(0, "Play the new DWARVES group game", 10.0f);
+    list.addItem(0, "Champitoad", -20.0f);
+    list.addItem(0, "Lucas8", 20.0f);
+    list.addItem(0, "Second", -50.0f);
+    list.addItem(0, "First");
+
+    /* Loading the gui */
+    gui::Gui g(gfx);
+    g.main(&list, geometry::Point(50.0f,50.0f), 700.0f, 500.0f);
 
     geometry::AABB bg;
     bg.width = 800.0f;
@@ -65,17 +83,15 @@ int main()
         ev.clearInput(); /* We don't need full input save, so we clear it */
         if(ev.isKeyPressed(events::KeyMap::Escape) || ev.isKeyPressed('q'))
             cont = false;
-        if(ev.keyJustPressed(events::KeyMap::Right))
-            item.scrollRight();
-        if(ev.keyJustPressed(events::KeyMap::Left))
-            item.scrollLeft();
-        if(ev.keyJustPressed(' '))
-            item.select(!item.selected());
+        g.update(ev);
+        if(ev.keyJustPressed(' ')) {
+            std::cout << std::dec << "Selected : \"" << list.selectedText() << "\", #" 
+                << list.selected() << " (id : " << std::hex << list.selectedID() << ")" << std::endl;
+        }
 
         gfx->beginDraw();
         gfx->draw(bg, bgc);
-        gfx->move(400.0f, 300.0f);
-        item.draw();
+        g.draw();
         gfx->endDraw();
         SDL_Delay(1000/60);
     }
