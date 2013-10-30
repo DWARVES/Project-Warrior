f3833b2fc131880e39b313c8609cea93ad98100d
lucas8 <luc.linux@mailoo.org>
2013-10-30 17:43:59 +0100

    Added a test of graphics::Graphics exposure to lua.

---
+++ tests/lua/gfx.lua
@@ -0,0 +1,68 @@
+
+gfx = Graphics()
+pressed = false
+act = false
+angle = 0
+
+init = function()
+    print("Init on namespace : ", gfx.namespace())
+    if not gfx.loadTexture("img", "img.png") then
+        print("Couldn't load img.png")
+        return false
+    end
+    gfx.hotpoint("img", 0, 25);
+
+    if not gfx.loadTexture("text", "text.png") then
+        print("Couldn't load text.png")
+        return false
+    end
+
+    if not gfx.loadFont("font", "font.png") then
+        print("Couldn't load font.png")
+        return false
+    end
+
+    if not gfx.loadMovie("movie", "movie") then
+        print("Couldn't load movie")
+        return false
+    end
+    return true
+end
+
+press = function()
+    pressed = true
+    act = not act
+    print("Pressed !")
+end
+
+unpress = function()
+    pressed = false
+    print("Unpressed !")
+end
+
+update = function()
+    gfx.push()
+    if not gfx.play("movie", 800, 600, true) then
+        gfx.rewind("movie")
+    end
+    gfx.move(300,200)
+    gfx.drawRect("text", 200, 200, 5, 3)
+    gfx.move(100,100);
+
+    if pressed then
+        gfx.drawText("Hello world", "font", 30)
+    end
+
+    gfx.push()
+    if act then
+        angle = angle + 1
+        while angle >= 360 do
+            angle = angle - 360
+        end
+    end
+    gfx.rotate(angle)
+    gfx.scale(5, 1)
+    gfx.blit("img")
+    gfx.pop()
+    gfx.pop()
+end
