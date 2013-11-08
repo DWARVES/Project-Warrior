14ab2f79d324b04e227ba3dc9d43c248bfb5ce57
lucas8 <luc.linux@mailoo.org>
2013-11-08 22:34:08 +0100

    Updated gui::Button test.

--- tests/gui/button-test.cpp
+++ tests/gui/button-test.cpp
@@ -16,18 +16,15 @@
 class ButtonTest : public gui::Button
 {
     public:
-        ButtonTest(graphics::Graphics* gfx, const std::string& name)
-            : Button(gfx), m_name(name)
+        ButtonTest(graphics::Graphics* gfx)
+            : Button(gfx)
         {}
         virtual ~ButtonTest()
         {}
 
         virtual void select() {
-            std::cout << "Selected \"" << m_name << "\" button." << std::endl;
+            std::cout << "Selected \"" << text() << "\" button." << std::endl;
         }
-
-    private:
-        std::string m_name;
 };
 
 int main()
@@ -49,15 +46,63 @@ int main()
         return 1;
 
     /* Setting up the buttons */
-    gfx->loadTexture("un",  "guirc/button/un.png");
-    gfx->loadTexture("foc", "guirc/button/foc.png");
-    gfx->loadTexture("sel", "guirc/button/sel.png");
-    ButtonTest b00(gfx, "0x0");
-    b00.setTextures("un", "foc", "sel");
-    ButtonTest b01(gfx, "0x1");
-    b01.setTextures("un", "foc", "sel");
-    ButtonTest b1(gfx, "1x*");
-    b1.setTextures("un", "foc", "sel");
+    gfx->loadTexture("r",  "guirc/button/r.png");
+    gfx->loadTexture("m",  "guirc/button/m.png");
+    gfx->loadTexture("l",  "guirc/button/l.png");
+    gfx->loadFont   ("f",  "guirc/button/f.png");
+    gfx->loadTexture("rf", "guirc/button/rf.png");
+    gfx->loadTexture("mf", "guirc/button/mf.png");
+    gfx->loadTexture("lf", "guirc/button/lf.png");
+    gfx->loadFont   ("ff", "guirc/button/ff.png");
+    gfx->loadTexture("rs", "guirc/button/rs.png");
+    gfx->loadTexture("ms", "guirc/button/ms.png");
+    gfx->loadTexture("ls", "guirc/button/ls.png");
+    gfx->loadFont   ("fs", "guirc/button/fs.png");
+    ButtonTest b00(gfx);
+    b00.text("0x0");
+    b00.maxSize(geometry::AABB(-1.0f, 100.0f));
+    ButtonTest b01(gfx);
+    b01.text("0x1");
+    b01.maxSize(geometry::AABB(-1.0f, 100.0f));
+    ButtonTest b1(gfx);
+    b1.text("1x*");
+    b1.maxSize(geometry::AABB(-1.0f, 100.0f));
+    b00.setTexture(gui::Button::Right,  false, "r");
+    b00.setTexture(gui::Button::Middle, false, "m");
+    b00.setTexture(gui::Button::Left,   false, "l");
+    b00.setTexture(gui::Button::Font,   false, "f");
+    b00.setTexture(gui::Button::Right,  true,  "rs");
+    b00.setTexture(gui::Button::Middle, true,  "ms");
+    b00.setTexture(gui::Button::Left,   true,  "ls");
+    b00.setTexture(gui::Button::Font,   true,  "fs");
+    b00.setSel(gui::Button::Right,  "rf");
+    b00.setSel(gui::Button::Middle, "mf");
+    b00.setSel(gui::Button::Left,   "lf");
+    b00.setSel(gui::Button::Font,   "ff");
+    b01.setTexture(gui::Button::Right,  false, "r");
+    b01.setTexture(gui::Button::Middle, false, "m");
+    b01.setTexture(gui::Button::Left,   false, "l");
+    b01.setTexture(gui::Button::Font,   false, "f");
+    b01.setTexture(gui::Button::Right,  true,  "rs");
+    b01.setTexture(gui::Button::Middle, true,  "ms");
+    b01.setTexture(gui::Button::Left,   true,  "ls");
+    b01.setTexture(gui::Button::Font,   true,  "fs");
+    b01.setSel(gui::Button::Right,  "rf");
+    b01.setSel(gui::Button::Middle, "mf");
+    b01.setSel(gui::Button::Left,   "lf");
+    b01.setSel(gui::Button::Font,   "ff");
+    b1.setTexture(gui::Button::Right,  false, "r");
+    b1.setTexture(gui::Button::Middle, false, "m");
+    b1.setTexture(gui::Button::Left,   false, "l");
+    b1.setTexture(gui::Button::Font,   false, "f");
+    b1.setTexture(gui::Button::Right,  true,  "rs");
+    b1.setTexture(gui::Button::Middle, true,  "ms");
+    b1.setTexture(gui::Button::Left,   true,  "ls");
+    b1.setTexture(gui::Button::Font,   true,  "fs");
+    b1.setSel(gui::Button::Right,  "rf");
+    b1.setSel(gui::Button::Middle, "mf");
+    b1.setSel(gui::Button::Left,   "lf");
+    b1.setSel(gui::Button::Font,   "ff");
 
     /* Setting up the gridlayout */
     gui::GridLayout layout(gfx);
