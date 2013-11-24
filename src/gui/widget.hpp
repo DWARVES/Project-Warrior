65b4a6edd694c55a7cc905682abd33b4758bcb0c
lucas8 <luc.linux@mailoo.org>
2013-11-24 14:37:56 +0100

    Documented gui::Widget.

--- src/gui/widget.hpp
+++ src/gui/widget.hpp
@@ -8,46 +8,54 @@
 
 namespace gui
 {
+    /** @brief Pure virtual class representing an element of the gui (a widget). */
     class Widget
     {
         public:
             Widget() = delete;
             Widget(const Widget&) = delete;
+            /** @brief Every widgets needs an instance of graphics::Graphics to draw themselves. */
             Widget(graphics::Graphics* gfx);
             virtual ~Widget();
 
-            /* Set size of the widget */
+            /** @brief Set width of the widget. */
             virtual float width(float w);
+            /** @brief Set height of the widget. */
             virtual float height(float h);
-            /* Get size of the widget */
+            /** @brief Get width of the widget. */
             virtual float width() const;
+            /** @brief Get height of the widget. */
             virtual float height() const;
 
-            /* Will be called at every loop
-             * Consider the origin to be the top-left of your Widget
+            /** @brief Draw the widget.
+             *
+             * Before call, the origin is placed at the top-left corner of the widget.
+             * This call shouldn't leave the origin changed.
              */
             virtual void draw() = 0;
 
-            /* Actions */
+            /** @brief Events handled by widgets. */
             enum Action {
-                First,
-                Last,
-                ScrollLeft,
-                ScrollRight,
-                ScrollUp,
-                ScrollDown,
-                Select,
-                Remove,
+                First,       /**< @brief Request to go to the beginning/first part. */
+                Last,        /**< @brief Request to go to the end/last part. */
+                ScrollLeft,  /**< @brief Request to go to the left. */
+                ScrollRight, /**< @brief Request to go to the right. */
+                ScrollUp,    /**< @brief Request to go upward. */
+                ScrollDown,  /**< @brief Request to go downward. */
+                Select,      /**< @brief Request selection from the widget. */
+                Remove,      /**< @brief Request removal from the widget. */
             };
-            /* The following functions do nothing,
-             * If your widget uses them, override them with your own implementation
-             */
+
+            /** @brief Change the focus state of the widget. */
             virtual void focus(bool);
+            /** @brief Text typed while the widget was focused. */
             virtual void inputText(const std::string&);
-            /* Return true if the action was handled */
+            /** @brief Widget::Action sended to the widget.
+             * @return True if the action was handled. */
             virtual bool action(Action);
 
         protected:
+            /** @brief The graphics::Graphics instance used by the widget. */
             graphics::Graphics* m_gfx;
 
         private:
