81a01c4e4fb357b591756d33dbd9ad0213ed7053
lucas8 <luc.linux@mailoo.org>
2013-11-24 01:44:29 +0100

    Documented audio::internal::Sound.

--- src/audio/sound.hpp
+++ src/audio/sound.hpp
@@ -9,17 +9,20 @@ namespace audio
 {
     namespace internal
     {
+        /** @brief Represents a sound. */
         class Sound
         {
             public:
                 Sound();
                 Sound(const Sound&) = delete;
                 ~Sound();
+                /** @brief Loads the sound from a file, accepts any format handled by Mix_LoadWAV. */
                 bool load(const std::string& path);
+                /** @brief Provides direct access to the Mix_Chunk m_snd pointer. */
                 Mix_Chunk* get() const;
 
             private:
-                Mix_Chunk* m_snd;
+                Mix_Chunk* m_snd; /**< The sound stored by SDL_Mixer. */
         };
     }
 }
