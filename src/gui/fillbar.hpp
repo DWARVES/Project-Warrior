3e84c01fa194001f8cb56313e078afbe82cc122e
lucas8 <luc.linux@mailoo.org>
2013-11-27 15:31:29 +0100

    Documented gui::Fillbar.

--- src/gui/fillbar.hpp
+++ src/gui/fillbar.hpp
@@ -7,6 +7,7 @@
 
 namespace gui
 {
+    /** @brief A widget represinting a fill bar. */
     class FillBar : public Widget
     {
         public:
@@ -16,21 +17,39 @@ namespace gui
             virtual ~FillBar();
 
             /* Values */
+            /** @brief Set the total size. */
             float size(float s);
+            /** @brief Get the total size. */
             float size() const;
+            /** @brief Set the filled size. */
             float set(float f);
+            /** @brief Get the filled size. */
             float get() const;
+            /** @brief Get the filled percent. */
             float percent() const;
+            /** @brief Set the steps used in interactive mode. */
             float step(float s);
+            /** @brief Get the steps used in interactive mode. */
             float step() const;
+            /** @brief Set the ratio, height/width. */
             float ratio(float r);
+            /** @brief Get the ratio. */
             float ratio() const;
+            /** @brief Set the direction, true meaning vertical. */
             bool dir(bool v);
+            /** @brief Is the bar drawn vertically. */
             bool vert() const;
+            /** @brief Set the interactive mode, if false, al Widget::Action are ignored. */
             bool interactive(bool i);
+            /** @brief Is in interactive mode. */
             bool interactive() const;
 
             /* Textures */
+            /** @brief Set the textures used.
+             * @param f Are the texture to be used when focused.
+             * @param empty The texture of the empty fillbar.
+             * @param filled The texture of the filled fillbar.
+             */
             void setTexture(bool f, const std::string& empty, const std::string& filled);
 
             /* Drawing */
@@ -41,14 +60,14 @@ namespace gui
             virtual bool action(Widget::Action a);
 
         private:
-            unsigned int m_focus;
-            bool m_vert;
-            bool m_interact;
-            float m_size;
-            float m_filled;
-            float m_step;
-            float m_ratio;
-            std::string m_texts[2][2];
+            unsigned int m_focus;      /** @brief The index of the focused texture. */
+            bool m_vert;               /** @brief Must it be drawn vertically. */
+            bool m_interact;           /** @brief Is the interactive mode enabled. */
+            float m_size;              /** @brief The total size. */
+            float m_filled;            /** @brief The size of the filled part. */
+            float m_step;              /** @brief The step used in interactive mode. */
+            float m_ratio;             /** @brief The ration, height/width. */
+            std::string m_texts[2][2]; /** @brief The textures, [focus][empty/filled]. */
     };
 }
 
