c7a1ef9ed96871286e7c3a9e6beafd9bc0bd7025
lucas8 <luc.linux@mailoo.org>
2013-12-11 20:48:07 +0100

    Added a class in events to represent a personnalised events.

---
+++ src/events/evsave.cpp
@@ -0,0 +1,48 @@
+
+#include "evsave.hpp"
+#include "events.hpp"
+#include <boost/regex.hpp>
+
+namespace events
+{
+    std::map<std::string,EvSave*> EvSave::m_registered;
+
+    EvSave::EvSave()
+    {}
+
+    EvSave::~EvSave()
+    {}
+
+    void EvSave::regist(EvSave* sv)
+    {
+        if(m_registered.find(sv->id()) != m_registered.end())
+            return;
+        else
+            m_registered[sv->id()] = sv;
+    }
+
+    EvSave* EvSave::parse(const std::string& sv)
+    {
+        boost::regex type ("^\\s*\\((.+?)\\)");
+        boost::smatch results;
+        if(!boost::regex_match(sv, results, type))
+            return NULL;
+
+        if(m_registered.find(results[1]) == m_registered.end())
+            return NULL;
+        
+        EvSave* ev = m_registered[results[1]];
+        if(!ev->load(sv))
+            return NULL;
+        return ev->copy();
+    }
+
+    void EvSave::freeAll()
+    {
+        for(auto p : m_registered)
+            delete p.second;
+        m_registered.clear();
+    }
+
+}
+
---
+++ src/events/evsave.hpp
@@ -0,0 +1,49 @@
+
+#ifndef DEF_EVENTS_EVSAVE
+#define DEF_EVENTS_EVSAVE
+
+#include <map>
+#include <string>
+
+namespace events
+{
+    class Events;
+
+    /** @brief An abstract class that represent a saved event. */
+    class EvSave
+    {
+        public:
+            EvSave();
+            virtual ~EvSave();
+
+            /** @brief Returns true the event saved was done. */
+            virtual bool valid(const events::Events& ev) = 0;
+            /** @brief Indicates if the event is being holded. */
+            virtual bool valid() const = 0;
+            /** @brief Check if the event is still holded. */
+            virtual bool still(const events::Events& ev) = 0;
+            /** @brief Save the event to a string. */
+            virtual std::string save() const = 0;
+            /** @brief Load the event from a string. */
+            virtual bool load(const std::string& sv) = 0;
+            /** @brief Returns an ID corresponding of the type of event saved. */
+            virtual std::string id() const = 0;
+            /** @brief Allocates a new saved event with the same params and return it. */
+            virtual EvSave* copy() const = 0;
+
+            /* Static members */
+            /** @brief Register a new type of event. */
+            static void regist(EvSave* sv);
+            /** @brief Load a saved event. Returns NULL is the type of the event is not registered or if an error happened. */
+            static EvSave* parse(const std::string& sv);
+            /** @brief Free all the saved events registered. */
+            static void freeAll();
+
+        private:
+            /** @brief The registered types <id,type>. */
+            static std::map<std::string,EvSave*> m_registered;
+    };
+}
+
+#endif
+
