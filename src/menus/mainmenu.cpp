205c4c9c5ef7833a7a297a6c00d35e565ae2a0da
lucas8 <luc.linux@mailoo.org>
2013-12-15 20:04:38 +0100

    Began implementation of MainMenu.

---
+++ src/menus/mainmenu.cpp
@@ -0,0 +1,106 @@
+
+#include "mainmenu.hpp"
+#include "global.hpp"
+#include "core/i18n.hpp"
+#include <sstream>
+
+MainMenu::MainMenu()
+    : Menu(), m_play(NULL), m_cfg(NULL), m_about(NULL), m_layout(NULL), m_actual(NULL)
+{}
+
+MainMenu::~MainMenu()
+{
+    if(m_layout)
+        delete m_layout;
+    if(m_play)
+        delete m_play;
+    if(m_cfg)
+        delete m_cfg;
+    if(m_about)
+        delete m_about;
+}
+
+bool MainMenu::prepare()
+{
+    /* Load the texture if necessary. */
+    if(!global::gfx->enterNamespace("/mainmenu")) {
+        if(!global::gfx->createNamespace("/mainmenu"))
+            return false;
+        global::gfx->enterNamespace("/mainmenu");
+
+        std::ostringstream path;
+        path << global::cfg->get<std::string>("rcs") << "/mainmenu/";
+        if(!global::gfx->loadTexture("bg", path.str() + "bg.png"))
+            return false;
+        if(!global::gfx->loadTexture("title", path.str() + "title.png"))
+            return false;
+    }
+
+    /* Create the widgets if necessary. */
+    if(!m_layout) {
+        /** @todo Set the right menus. */
+        m_play = new ButtonMenu(global::gfx, &m_actual, new MainMenu, true);
+        m_play->text(_i("Play"));
+        global::theme->apply(m_play);
+
+        m_cfg = new ButtonMenu(global::gfx, &m_actual, new MainMenu, true);
+        m_cfg->text(_i("Config"));
+        global::theme->apply(m_cfg);
+
+        m_about = new ButtonMenu(global::gfx, &m_actual, new MainMenu, true);
+        m_about->text(_i("About"));
+        global::theme->apply(m_about);
+
+        m_layout = new gui::GridLayout(global::gfx, 2, 2);
+        global::theme->apply(m_layout);
+        m_layout->addWidget(m_play,  0, 0, 0, 1);
+        m_layout->addWidget(m_cfg,   1, 0, 0, 0);
+        m_layout->addWidget(m_about, 1, 1, 0, 0);
+    }
+
+    /* Set up the gui. */
+    geometry::Point pos;
+    global::gfx->disableVirtualSize();
+    pos.x = global::gfx->getVirtualWidth() * 0.1f;
+    pos.y = global::gfx->getVirtualHeight() * 0.2f;
+    global::gui->main(m_layout, pos, global::gfx->getVirtualWidth() * 0.8f, global::gfx->getVirtualHeight() * 0.7f);
+
+    return true;
+}
+
+bool MainMenu::update()
+{
+    if(m_actual) {
+        /** @todo Handle it right (actual code is for debug). */
+        std::cout << "Entered " << std::hex << m_actual << std::dec << " menu." << std::endl;
+        prepare();
+        m_actual = NULL;
+        return true;
+    }
+
+    global::gui->update(*global::evs);
+    if(global::evs->keyJustPressed(events::KeyMap::Escape))
+        return false;
+
+    /* Drawing */
+    global::gfx->enterNamespace("/mainmenu");
+    geometry::AABB rect(global::gfx->getVirtualWidth(), global::gfx->getVirtualHeight());
+
+    global::gfx->beginDraw();
+    global::gfx->push();
+    global::gfx->draw(rect, "bg");
+
+    rect.width *= 0.8f;
+    rect.height *= 0.1f;
+    global::gfx->move(global::gfx->getVirtualWidth() * 0.1f, 0.0f);
+    global::gfx->draw(rect, "title");
+    global::gfx->pop();
+
+    global::theme->guiNamespace();
+    global::gui->draw();
+    global::gfx->endDraw();
+
+    return true;
+}
+
+
---
+++ src/menus/mainmenu.hpp
@@ -0,0 +1,31 @@
+
+#ifndef DEF_MAINMENU
+#define DEF_MAINMENU
+
+#include "menus/menu.hpp"
+#include "menus/buttonmenu.hpp"
+#include "gui/gridlayout.hpp"
+#include "gui/button.hpp"
+
+/** @brief Manages the main menu.
+ * @todo Play music.
+ */
+class MainMenu : public Menu
+{
+    public:
+        MainMenu();
+        virtual ~MainMenu();
+
+        virtual bool prepare();
+        virtual bool update();
+
+    private:
+        ButtonMenu* m_play;        /**< @brief The button for the play menu. */
+        ButtonMenu* m_cfg;         /**< @brief The button for the config menu. */
+        ButtonMenu* m_about;       /**< @brief The button for the about menu. */
+        gui::GridLayout* m_layout; /**< @brief The layout. */
+        Menu* m_actual;            /**< @brief The actual submenu is in, NULL if none. */
+};
+
+#endif
+
