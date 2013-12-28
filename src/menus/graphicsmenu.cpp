52b0ae02f25b0c1b9cbd6819276c77da35e034d4
lucas8 <luc.linux@mailoo.org>
2013-12-28 15:00:34 +0100

    Finished implementation of GraphicsMenu.

--- src/menus/graphicsmenu.cpp
+++ src/menus/graphicsmenu.cpp
@@ -11,8 +11,10 @@ GraphicsMenu::GraphicsMenu()
 
 GraphicsMenu::~GraphicsMenu()
 {
-    if(m_res)
+    if(m_res) {
+        m_res->freeData([] (void* data) { delete (geometry::AABB*)data; });
         delete m_res;
+    }
     if(m_fs)
         delete m_fs;
     if(m_back)
@@ -40,6 +42,13 @@ bool GraphicsMenu::prepare()
 
         m_res = new gui::List(global::gfx);
         global::theme->apply(m_res);
+        /* Populate m_res. */
+        std::vector<geometry::AABB> res = global::gfx->windowRes(800, 600);
+        for(size_t i = 0; i < res.size(); ++i) {
+            std::ostringstream label;
+            label << res[i].width << "x" << res[i].height << std::endl;
+            m_res->addItem(i, label.str(), 0.0f, new geometry::AABB(res[i]));
+        }
 
         m_layout = new gui::GridLayout(global::gfx, 2, 3);
         global::theme->apply(m_layout);
@@ -47,15 +56,6 @@ bool GraphicsMenu::prepare()
         m_layout->addWidget(m_fs,      1, 0, 0, 0);
         m_layout->addWidget(m_apply,   1, 1, 0, 0);
         m_layout->addWidget(m_back,    1, 2, 0, 0);
-    }
-
-    /* Populate m_res. */
-    m_res->clear();
-    std::vector<geometry::AABB> res = global::gfx->windowRes(800, 600);
-    for(size_t i = 0; i < res.size(); ++i) {
-        std::ostringstream label;
-        label << res[i].width << "x" << res[i].height << std::endl;
-        m_res->addItem(i, label.str());
     }
 
     /* Default value of m_fs. */
@@ -90,11 +90,15 @@ bool GraphicsMenu::update()
         /** @todo Reoppening the window with the same gl context. */
         if(m_fs->get()) {
             global::cfg->set<bool>("fullscreen", true);
+            global::gfx->setFullscreen(true);
         }
         else {
             global::cfg->set<bool>("fullscreen", false);
-            /** @todo Get the selected size. */
+            global::gfx->setFullscreen(false);
+            geometry::AABB size = *(geometry::AABB*)m_res->selectedData();
+            global::gfx->windowSize((int)size.width, (int)size.height);
         }
+        global::gfx->disableVirtualSize();
 
         return false;
     }
