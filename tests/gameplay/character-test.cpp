8b7c6dba80dbfcf4bdd1d387da48f5a75499c3cd
lucas8 <luc.linux@mailoo.org>
2014-01-19 17:39:54 +0100

    Added a test for gameplay::Character.

---
+++ tests/gameplay/character-test.cpp
@@ -0,0 +1,102 @@
+
+#include <SDL.h>
+#include <iostream>
+#include <sstream>
+#include "core/logger.hpp"
+#include "graphics/graphics.hpp"
+#include "events/events.hpp"
+#include "gameplay/character.hpp"
+#include "lua/graphicsExposure.hpp"
+#include "global.hpp"
+
+namespace global {
+    graphics::Graphics* gfx;
+}
+
+int main()
+{
+    core::logger::init();
+    core::logger::addOutput(&std::cout);
+    graphics::Graphics* gfx = new graphics::Graphics;
+    global::gfx = gfx;
+    lua::exposure::Graphics::setGraphicsInstance(gfx);
+
+    bool cont = false;
+    events::Events evs;
+    evs.enableInput(false);
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "Couldn't load SDL : " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    cont = gfx->openWindow("Test graphics::Graphics", 800, 600);
+    if(!cont)
+        return 1;
+
+    gameplay::Character* chara = new gameplay::Character("rcs/chara/stick");
+    if(!chara->preload()) {
+        core::logger::logm("Couldn't load the character preview.", core::logger::FATAL);
+        return 1;
+    }
+    std::cout << "Character **" << chara->name() << "** loaded : " << chara->desc() << std::endl;
+
+    if(!chara->load(gameplay::Character::None)) {
+        core::logger::logm("Couldn't load the character.", core::logger::FATAL);
+        return 1;
+    }
+
+    geometry::AABB bg(800, 600);
+    graphics::Color bgc(0, 255, 255);
+    auto dir = gameplay::Character::Fixed;
+
+    while(cont) {
+        evs.update();
+        if(evs.closed() || evs.quit() || evs.isKeyPressed(events::KeyMap::Escape))
+            cont = false;
+
+        if(evs.isKeyPressed(events::KeyMap::Left))
+            dir = gameplay::Character::Left;
+        else if(evs.isKeyPressed(events::KeyMap::Right))
+            dir = gameplay::Character::Right;
+        else if(evs.isKeyPressed(events::KeyMap::Up))
+            dir = gameplay::Character::Up;
+        else if(evs.isKeyPressed(events::KeyMap::Down))
+            dir = gameplay::Character::Down;
+        else
+            dir = gameplay::Character::Fixed;
+
+        if(evs.isKeyPressed(events::Key('a')))
+            chara->action(gameplay::Character::Attack, dir);
+        else if(evs.isKeyPressed(events::Key('z'))) {
+            std::cout << "Spell !" << std::endl;
+            chara->action(gameplay::Character::Spell, dir);
+        }
+        else if(evs.isKeyPressed(events::Key('e')))
+            chara->action(gameplay::Character::Smash, dir);
+        else if(evs.isKeyPressed(events::Key('q')))
+            chara->action(gameplay::Character::Dodge, dir);
+        else if(evs.isKeyPressed(events::Key('e')))
+            chara->action(gameplay::Character::DDodge, dir);
+        else if(evs.isKeyPressed(events::Key('d')))
+            chara->action(gameplay::Character::Catch, dir);
+        else if(evs.isKeyPressed(events::Key(' ')))
+            chara->action(gameplay::Character::Run, dir);
+        else
+            chara->action(gameplay::Character::Walk, dir);
+
+        gfx->beginDraw();
+        gfx->draw(bg, bgc);
+        chara->draw();
+        gfx->endDraw();
+
+        SDL_Delay(1000/30);
+    }
+
+    delete chara;
+    delete gfx;
+    core::logger::free();
+    SDL_Quit();
+    return 0;
+}
+
