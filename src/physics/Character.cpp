517dee12e98871d713c8d9b23ac42457a4e48f1f
lucas8 <luc.linux@mailoo.org>
2014-03-31 22:11:59 +0200

    Fixed a dimension bug about Character physical ground detector.

--- src/physics/Character.cpp
+++ src/physics/Character.cpp
@@ -19,8 +19,8 @@ namespace physics
     {
         createFixture("body", rect, weight, 1);
 
-        geometry::AABB feet(rect.width, 10);
-        geometry::Point pos(0, rect.height / 2);
+        geometry::AABB feet(rect.width, rect.height * 0.05f);
+        geometry::Point pos(0, -rect.height / 2);
         b2Fixture* sens = createFixture("feet", feet, 1, 1, Type::ThisType, Type::ThisCollideWith, pos, true);
         world->setCallback(this, sens, character_foot_callback);
     }
