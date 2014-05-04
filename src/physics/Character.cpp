b46cd138bd33e6f1660520051a1308da01a2fd2d
lucas8 <luc.linux@mailoo.org>
2014-05-05 00:34:05 +0200

    Sensors don't count as ground anymore in physics::Character.

--- src/physics/Character.cpp
+++ src/physics/Character.cpp
@@ -22,7 +22,7 @@ namespace physics
         geometry::AABB feet(rect.width * 0.99f, rect.height * 0.05f);
         geometry::Point pos(0, -rect.height / 2);
         b2Fixture* sens = createFixture("feet", feet, 1, 1, Type::ThisType, Type::ThisCollideWith, pos, true);
-        world->setCallback(this, sens, character_foot_callback);
+        world->setCallback(this, sens, character_foot_callback, NULL, false);
     }
 
     Character::~Character()
