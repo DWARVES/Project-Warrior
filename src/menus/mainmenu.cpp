bd6df6075170e5a506eef43622f7f4fb585e35e7
lucas8 <luc.linux@mailoo.org>
2014-05-29 20:49:36 +0200

    Removed main menu interface.

--- src/menus/mainmenu.cpp
+++ src/menus/mainmenu.cpp
@@ -6,11 +6,11 @@
 #include "lua/charaExposure.hpp"
 #include <sstream>
 
-const char* ctrl0 = "keyboard";
+const char* ctrl0 = "Keyboard";
 const char* ctrl1 = "Mega World Thrustmaster dual analog 3.2";
 
-MainMenu::MainMenu()
-    : Menu(), m_play(NULL), m_quit(NULL), m_layout(NULL), m_actual(NULL)
+    MainMenu::MainMenu()
+: Menu(), m_actual(NULL)
 {
     for(int i = 0; i < 4; ++i)
         m_ctrls[i] = NULL;
@@ -18,12 +18,6 @@ MainMenu::MainMenu()
 
 MainMenu::~MainMenu()
 {
-    if(m_layout)
-        delete m_layout;
-    if(m_play)
-        delete m_play;
-    if(m_quit)
-        delete m_quit;
     if(m_st)
         delete m_st;
     for(int i = 0; i < 4; ++i) {
@@ -34,85 +28,14 @@ MainMenu::~MainMenu()
 
 bool MainMenu::prepare()
 {
-    /* Load the music if necessary. */
-    if(!global::audio->existsNamespace("/menus")) {
-        if(!global::audio->createNamespace("/menus"))
-            return false;
-        global::audio->enterNamespace("/menus");
-
-        std::ostringstream path;
-        path << global::cfg->get<std::string>("rcs") << "/sounds/menu.music";
-        if(!global::audio->loadMusic("mus", path.str()))
-            return false;
-    }
-    global::audio->enterNamespace("/menus");
-    global::audio->play("mus", false);
-
-    if(!loadRcs())
-        return false;
-
-    /* Create the widgets if necessary. */
-    if(!m_layout) {
-        m_play = new gui::Button(global::gfx);
-        m_play->text(_i("Play"));
-        global::theme->apply(m_play);
-
-        m_quit = new gui::Button(global::gfx);
-        m_quit->text(_i("Quit"));
-        global::theme->apply(m_quit);
-
-        m_layout = new gui::GridLayout(global::gfx, 3, 3);
-        global::theme->apply(m_layout);
-        m_layout->addWidget(m_play,  0, 0, 2, 1);
-        m_layout->addWidget(m_quit,  0, 2, 2, 0);
-    }
-
-    /* Set up the gui. */
-    geometry::Point pos;
-    global::gfx->disableVirtualSize();
-    pos.x = global::gfx->getVirtualWidth() * 0.1f;
-    pos.y = global::gfx->getVirtualHeight() * 0.2f;
-    global::gui->main(m_layout, pos, global::gfx->getVirtualWidth() * 0.8f, global::gfx->getVirtualHeight() * 0.7f);
-
-    return true;
-}
-        
-bool MainMenu::loadRcs()
-{
-    /* Load the textures if necessary. */
-    if(!global::gfx->existsNamespace("/mainmenu")) {
-        if(!global::gfx->createNamespace("/mainmenu"))
-            return false;
-        global::gfx->enterNamespace("/mainmenu");
-
-        std::ostringstream path;
-        path << global::cfg->get<std::string>("rcs") << "/mainmenu/";
-        if(!global::gfx->loadTexture("bg", path.str() + "bg.png"))
-            return false;
-        if(!global::gfx->loadTexture("title", path.str() + "title.png"))
-            return false;
-    }
-
     return true;
 }
 
 bool MainMenu::update()
 {
-    if(m_actual) {
-        if(m_actual->update())
-            return true;
-        global::gfx->disableVirtualSize();
-        global::gfx->invertYAxis(false);
-        prepare();
-        m_actual = NULL;
-        return true;
-    }
-
-    if(global::evs->keyJustPressed(events::KeyMap::Escape)
-            || m_quit->clicked())
-        return false;
-
-    if(m_play->clicked()) {
+    if(m_actual)
+        return m_actual->update();
+    else {
         /* Creating the controlers. */
         for(int i = 0; i < 4; ++i) {
             if(m_ctrls[i])
@@ -150,24 +73,6 @@ bool MainMenu::update()
         if(!m_actual->prepare())
             return false;
     }
-
-    /* Drawing */
-    global::gfx->enterNamespace("/mainmenu");
-    geometry::AABB rect(global::gfx->getVirtualWidth(), global::gfx->getVirtualHeight());
-
-    global::gfx->beginDraw();
-    global::gfx->push();
-    global::gfx->draw(rect, "bg");
-
-    rect.width *= 0.8f;
-    rect.height *= 0.1f;
-    global::gfx->move(global::gfx->getVirtualWidth() * 0.1f, 0.0f);
-    global::gfx->draw(rect, "title");
-    global::gfx->pop();
-
-    global::theme->guiNamespace();
-    global::gui->draw();
-    global::gfx->endDraw();
 
     return true;
 }
--- src/menus/mainmenu.hpp
+++ src/menus/mainmenu.hpp
@@ -3,8 +3,6 @@
 #define DEF_MAINMENU
 
 #include "menus/menu.hpp"
-#include "gui/gridlayout.hpp"
-#include "gui/button.hpp"
 #include "gameplay/controler.hpp"
 #include "gameplay/character.hpp"
 #include "gameplay/stage.hpp"
@@ -22,9 +20,6 @@ class MainMenu : public Menu
         static bool loadRcs();
 
     private:
-        gui::Button* m_play;       /**< @brief The button launching the game. */
-        gui::Button* m_quit;       /**< @brief The button for quitting. */
-        gui::GridLayout* m_layout; /**< @brief The layout. */
         Menu* m_actual;            /**< @brief The actual submenu is in, NULL if none. */
         gameplay::Controler* m_ctrls[4];
         gameplay::Stage* m_st;
